ce8d9366f13a637ae9b015bb722e457eb77e4c85
Erin M. Ochoa <erin@Erins-MacBook-Pro.local>
2017-02-08 21:53:32 -0600

    Done with recessions scraper

--- code/scrapers/recessions-emo.py
+++ code/scrapers/recessions-emo.py
@@ -2,10 +2,10 @@ import os
 import re
 import bs4
 import requests
+import numpy as np
 
 OUTPUT_DIR = 'recessions_data'
-OUTPUT_START = 'recession_start_dates'
-OUTPUT_END = 'recession_end_dates'
+OUTPUT_FILE = 'recession_dates.csv'
 URL = 'http://www.nber.org/cycles.html'
 
 def create_output_dir():
@@ -58,21 +58,71 @@ def process_page(soup):
             start_dates.append(line)
         for line in lines[44:78]:
             end_dates.append(line)
-    return start_dates, end_dates
+
+    clean_start = clean_dates(start_dates)
+    clean_start.insert(0,[''])
+
+    clean_end = clean_dates(end_dates)
+
+    return clean_start, clean_end
 
 
 def clean_dates(dates):
     '''
     '''
 
+    expr = '[A-Z][a-z]*[\s]+[1-2][0-9][0-9][0-9]'
+
     clean_dates = []
 
     for date in dates:
-        clean_date = re.findall('[A-Z][a-z]*[\s]+[1-2][0-9][0-9][0-9]', date)
-        clean_dates.append(clean_date)
+        date_string = re.findall(expr, date)[0]
+        month, year = date_string.split()
+        clean_date = year + ' ' + month
+        clean_dates.append([clean_date])
 
     return clean_dates
 
+
+
+def build_array(clean_start, clean_end):
+    '''
+    '''
+    start = np.array(clean_start)
+    end = np.array(clean_end)
+
+    array = np.hstack((start, end))
+
+    return array
+
+
+
+def write_csv(array):
+    '''
+    '''
+
+    headers = 'Start_Date,End_Date\n'
+
+    path = os.path.join(OUTPUT_DIR, OUTPUT_FILE)
+
+    csv_file = open(path, 'w')
+
+    csv_file.write(headers)
+
+    for line in array[:-1]:
+        start = line[0]
+        end = line[1]
+        string = start + ',' + end + '\n'
+        csv_file.write(string)
+
+    start = array[-1][0]
+    end = array[-1][1]
+
+    string = start + ',' + end
+
+    csv_file.write(string)
+
+    csv_file.close()
 
 
 
@@ -80,8 +130,8 @@ create_output_dir()
 
 soup = get_page(URL)
 
-start_dates, end_dates = process_page(soup)
+clean_start, clean_end = process_page(soup)
 
-#start_dates = clean_dates(start_dates)
+array = build_array(clean_start, clean_end)
 
-#end_dates = clean_dates(end_dates)
+write_csv(array)
