19ddde370fe9bbce4258cfd7b5c981cc1d443514
Anne Rogers <amr@cs.uchicago.edu>
2017-03-12 00:42:51 -0600

    wbdata final

--- code/scrapers/wbdata.py
+++ code/scrapers/wbdata.py
@@ -1,9 +1,10 @@
 import requests
 import pandas as pd
 import itertools
+import os
 
 
-BASIC_FORMAT = 'http://api.worldbank.org/'
+DOMAIN_NAME = 'http://api.worldbank.org/'
 
 COUNTRY_CODES = {'United States': 'USA', 'China': 'CHN','Germany':'DEU', 
                  'Japan': 'JPN', 'United Kingdom': 'GBR', 'India': 'IND', 
@@ -18,10 +19,18 @@ COMMODITIES = {'coffee': 'COFFEE_ROBUS', 'agriculture': 'IAGRICULTURE', 'beverag
                 'brent oil': 'CRUDE_BRENT', 'sugar': 'SUGAR_WLD', 'grains': 'IGRAINS', 
                 'gold': 'GOLD', 'tabacoo': 'TOBAC_US'}
 
-# create a country lookup function/class..
+#########################################
+# This module downloads data using      #
+# world bank's api structure. It        #
+# downloads 40 economic indicators from # 
+# GEM (Global  Economi Monitor) data    #
+# catalog and 11 commodities from GEM   #
+# Commodities data catalog.             #
+#########################################
 
 def create_request_object(myurl):
     '''
+    Take an url and create a request object. 
     '''
     r = requests.get(url=myurl)
     return r
@@ -29,24 +38,28 @@ def create_request_object(myurl):
 
 def get_countries():
     '''
+    Return a set of countries supported
     '''
     return set(COUNTRY_CODES)
 
 
 def get_indicators():
     '''
+    Return a set of economic indicators supported
     '''
     return set(INDICATORS)
 
 
 def get_commodities():
     '''
+    Returan a set of commodities supported
     '''
     return set(COMMODITIES)
 
 
 def build_country_code(country):
     '''
+    Build the url path that specifies a country. 
     '''
     if country in COUNTRY_CODES:
         country_code = COUNTRY_CODES[country]
@@ -59,6 +72,7 @@ def build_country_code(country):
 
 def build_indicator_code(indicator):
     '''
+    Build the url path that specifies an indicator. 
     '''
     if indicator in INDICATORS:
         indicator_code = INDICATORS[indicator]
@@ -71,6 +85,7 @@ def build_indicator_code(indicator):
 
 def build_commodity_indicator_code(commodity):
     '''
+    Build the url path that specifies a commodity. 
     '''
     if commodity in COMMODITIES:
         commodity_code = COMMODITIES[commodity]
@@ -83,7 +98,14 @@ def build_commodity_indicator_code(commodity):
 
 def define_date(yymm=None):
     '''
-    Take a tuple of six-digit year and month integers
+    Take a tuple of six-digit year and month integers, 
+    which represents start and end date, and return 
+    a proper date query format for world bank api.
+
+    Example input: (198001, 201612)
+
+    If no parameter is passed, it downloads data from 1960-01 to now 2017-03, 
+    1960 is the starting record of world bank GEM-Commodities data catalog. 
     '''
     if yymm is None:
         date_code = '1960M01:2017M03'
@@ -99,32 +121,61 @@ def define_date(yymm=None):
 
 def build_api_link(country, indicator, yymm=None, commodities=False):
     '''
+    Build the api link to the data of a desired desired indicator. 
+    Take a country name, an indicator name, start and end date (optional), 
+    and whether the indicator is a commodity or not. Return a link.  
+    
+    Inputs: 
+        country: a country label (example: 'Germany')
+        indicator: an indicator label (example: 'coffee')
+        yymm: (a tuple of integers )start and end year-month 
+              Example: (198001, 201612)
+        commodities: by default is False. Commodities are also indicators 
+                     in world bank's api structure. But they do not have by country 
+                     values, only all countries values. So the logic to build
+                     api link is a bit different. 
+   
+    Returns:
+        (str) a complete link
+
     '''
     date = define_date(yymm)
 
     if commodities:
         commodity = build_commodity_indicator_code(indicator)
-        return BASIC_FORMAT + commodity + '?format=json' + date + '&per_page=5000'
+        return DOMAIN_NAME + commodity + '?format=json' + date + '&per_page=5000'
     
     country = build_country_code(country)
     indicator = build_indicator_code(indicator)
     
-    api = BASIC_FORMAT + country + indicator + '?format=json' + date + '&per_page=5000' 
-    #better make a page-turning algorithm
+    api = DOMAIN_NAME + country + indicator + '?format=json' + date + '&per_page=5000' 
 
     return api 
 
 
 def load_data(country, indicator, yymm=None, commodities=False):
     '''
+    Take a country name, an indicator name, a start-end date pair, and 
+    evaluation of the indicator is a commodity or not, build the api link, 
+    download and decode JSON data. 
     '''
     myurl = build_api_link(country, indicator, yymm, commodities)
     r = create_request_object(myurl)
-    return r.json()  #check the string r.content
+    return r.json()
 
     
 def crawl_data(country, indicator, yymm=None, commodities=False):
     '''
+    Take a country, an indicator name, a start-end date pair, and evaluation 
+    of whether the indicator is a commodity or not, and return pared data. 
+
+    Returns:
+        indicator: official name of the indicator in world bank's dataset
+        country: which country the data is about
+        val: a list of indicator values
+        date: a list of date (year-month format) corresponding to 
+              the values above
+
     '''
     content = load_data(country, indicator, yymm, commodities)
     data = content[1]
@@ -144,7 +195,20 @@ def crawl_data(country, indicator, yymm=None, commodities=False):
 
 def select_gem_vars(country_lst=None, indicator_lst=None):
     '''
-    Select explanatory variables from world bank's GEM datalog
+    Select economic indicators (used as explanatory variables) 
+    from world bank's GEM datalog. 
+
+    Inputs:
+        If no parameters are passed, it will select a combination of all
+        available countries and indicators. 
+        (Cartetian product of 8 countries * 5 indicators = 40 variables)
+        country_lst: a selected list of countries 
+        indicator_lst: a selected list of indicators
+
+    Returns:
+        a list of economic indicators (country+indicator) from GEM datacatelog
+        Example: [('Japan', 'stock'), ('World', 'industrial production')]
+
     '''
     if country_lst is None:
         country_lst = get_countries()
@@ -158,6 +222,12 @@ def select_gem_vars(country_lst=None, indicator_lst=None):
 def create_predictors_df(country_lst=None, indicator_lst=None, yymm=None, \
                          outfile='./worldbank/gem.csv'):
     '''
+    Create a DataFrame of predictors and save into a csv file. By default download
+    all possible predictors supported by this module. 
+
+    Optional inputs:
+        a list of countries, a list of indicators, start-end date, and output
+        filename. 
     '''
     variables = select_gem_vars(country_lst, indicator_lst)
     
@@ -184,6 +254,9 @@ def create_predictors_df(country_lst=None, indicator_lst=None, yymm=None, \
 def create_commodities_df(commodity_lst=None, yymm=None, \
                          outfile='./worldbank/gem-commodities.csv'):
     '''
+    Create a DataFrame of commodities and save into a csv file. This function
+    by default downloads all 11 available commodities, spanning all available
+    time period from 1960-01 to 2017-03. 
     '''
     if commodity_lst is None:
         commodities_lst = list(COMMODITIES)
@@ -207,6 +280,12 @@ def create_commodities_df(commodity_lst=None, yymm=None, \
 
 if __name__ == '__main__':
 
+    directory = './worldbank'
+
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
     create_predictors_df()
+
     create_commodities_df()
    
