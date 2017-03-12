9b09299c5181c8c32f7ff57f6057ae671ff8132b
Anne Rogers <amr@cs.uchicago.edu>
2017-03-11 21:12:10 -0600

    reporting update

--- code/analysis_and_plotting/reporting.py
+++ code/analysis_and_plotting/reporting.py
@@ -1,33 +1,55 @@
 import numpy as np
 
 from pylatex import Document, MiniPage, PageStyle, Section, Subsection, Tabular, \
-                    Math, TikZ, Axis, Plot, Figure, Matrix, LargeText, \
-                    MediumText, LineBreak
+                    Head, Foot, Axis, Plot, Figure, Matrix, LargeText, \
+                    MediumText, LineBreak, simple_page_number
 from pylatex.utils import italic, bold, NoEscape
+
 import os
+import time
 
 
-
-# order matters, 1. add title first
-# add main
-# insert a default pic
-
-class Report(object):
+class Report:
     """docstring for ClassName"""
     
-    def __init__(self, header_image, margin='1.0in', default_filepath='./reports/report'):
+    def __init__(self, margin='1.0in', default_filepath='./reports/report'):
         
 
-        geometry_options = {"margin": margin}
+        geometry_options = {'margin': margin, 'paperheight': '11in', \
+                            'paperwidth':'8.5in'}
+        
         self.doc = Document(default_filepath= default_filepath, \
-                            geometry_options=geometry_options)
+                            geometry_options=geometry_options, font_size='large')
         
-        graph_filename = os.path.join(os.path.dirname(__file__), 'commodities.jpg')
 
-        with self.doc.create(Figure(position='t!')) as graph:
-            graph.add_image(header_image, width='200px')
+    def add_headfoot(self, header_image):
+        '''
+        '''
+        header = PageStyle('header')
+        
+        today = time.strftime('%B %d %Y')
+        company = 'Ochoa Valdés-Ortiz Zhang Ltd.'
+
+        #add header
+        with header.create(Head('L')) as lheader:
+            with lheader.create(Figure(position='t!')) as graph:
+                graph.add_image(header_image, width='400px')
 
         
+        #left footer
+        with header.create(Foot('L')):
+            header.append(today)
+            header.append(LineBreak())
+            header.append(company)
+        
+        #right footer
+        with header.create(Foot('R')):
+            header.append(simple_page_number())
+
+        
+        self.doc.preamble.append(header)
+
+        self.doc.change_document_style('header')
 
     def set_title(self, title, subtitle):
         '''
@@ -37,33 +59,35 @@ class Report(object):
             self.doc.append(LineBreak())
             self.doc.append(MediumText(italic(bold(subtitle))))
 
+    def add_executive_summary(self):
+        '''
+        '''
+        pass
+
     def insert_graph(self, sec_name, graph_fname, caption):
         '''
         '''
         with self.doc.create(Section(sec_name)):
             #doc.append('add section text')
-            with self.doc.create(Figure(position='h!')) as graph:
-                graph.add_image(graph_fname, width='180px')
+            with self.doc.create(Figure(position='t!')) as graph:
+                graph.add_image(graph_fname, width='200px')
                 graph.add_caption(caption)
 
-    def insert_table(self, lag, independent_var, num_diff, R2, stat):
+    def insert_table(self, results):
         '''
         '''
-        #Rodrigo can give me this info as a dictionary
+        indie_var = ' '.join(results['independent_var'])
 
-        lag_var = ('Lag variables', lag)
-        indie_var = ('Independent variables', ' '.join(independent_var))
+        section = Section('The Model')
 
-        section = Section('The model')
-
-        table = Tabular('c p{5cm}')
+        table = Tabular('l p{10cm}')
         table.add_hline()
-        table.add_row(('Model', 'AR'))
-        table.add_row(lag_var, color='lightgray')
-        table.add_row(indie_var)
-        table.add_row(('Number of differences', num_diff), color='lightgray')
-        table.add_row((NoEscape('$R^2$'), R2))
-        table.add_row(('Durbin Watson Statistic', stat), color='lightgray')
+        table.add_row((bold('Model'), 'AR'))
+        table.add_row((bold('Lag variables'), results['lag']), color='lightgray')
+        table.add_row((bold('Independent variables'), indie_var))
+        table.add_row((bold('Number of differences'), results['num_diff']), color='lightgray')
+        table.add_row((NoEscape('\symbf{$R^2$}'), results['R2']))
+        table.add_row((bold('Durbin Watson Statistic'), results['stat']), color='lightgray')
         table.add_hline()
         
         section.append(LineBreak())
@@ -79,9 +103,16 @@ class Report(object):
 
 if __name__ == '__main__':
 
-    header_image = os.path.join(os.path.dirname(__file__), 'commodities.jpg')
+    # order matters, 1. add title first
 
-    r = Report(header_image)
+    header_image = os.path.join(os.path.dirname(__file__), 'commodities_2.jpg')
+
+    results = {'lag':1, 'R2': 0.99, 'stat': 5.66, 'num_diff': 4,\
+               'independent_var': ['apple', 'banana', 'pear', 'peach']}
+    
+    r = Report()
+
     r.set_title('Forecast', 'Wheat')
-    r.insert_table(1, ['apple', 'banana', 'pear', 'peach'], 4, 0.99, 5.66)
+    r.add_headfoot(header_image)
+    r.insert_table(results)
     r.generate_pdf()
