39a4b194885a9fb550c389c102997243d94d9f38
Erin M. Ochoa <erin@Erins-MacBook-Pro.local>
2017-03-13 15:58:45 -0500

    Done with doc styles; moved body of pkl2report into main block

--- code/analysis_and_plotting/pkl2report.py
+++ code/analysis_and_plotting/pkl2report.py
@@ -5,25 +5,26 @@ from subprocess import check_output, CalledProcessError
 
 
 PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'pickles/')
-
+ERROR = "Exception:  There has been a CalledProcessError, but the report\n /
+has still been generated."
 PICKLES = 10
 
 
+if __name__ == '__main__':
+
+    #Call the reporting module to build the reports
+    header_image = '../commodity-pic.jpg'
+
+    for i in range(PICKLES):
+        name = PATH + 'df_tuple' + str(i) + '.pkl'
+        df, dicto = pickle.load(open(name, "rb" ))
+        try:
+            reporting.build_report(df, dicto, header_image)
+        except CalledProcessError:
+            print(ERROR)
+            continue
 
 
-#Call the reporting module to build the reports
-header_image = '../commodity-pic.jpg'
-
-for i in range(PICKLES):
-    name = PATH + 'df_tuple' + str(i) + '.pkl'
-    df, dicto = pickle.load(open(name, "rb" ))
-    try:
-        reporting.build_report(df, dicto, header_image)
-    except CalledProcessError:
-        print("Exception:  There has been a CalledProcessError, but the report was still generated.")
-        continue
-
-
-for fname in os.listdir(reporting.PATH):
-    if fname.endswith('.tex') or fname.endswith('.aux') or fname.endswith('log'):
-        os.remove(fname)
+    for fname in os.listdir(reporting.PATH):
+        if fname.endswith('.tex') or fname.endswith('.aux') or fname.endswith('log'):
+            os.remove(fname)
